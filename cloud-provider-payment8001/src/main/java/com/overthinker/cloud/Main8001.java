000264c269ef4e82a5bb1434e70572cd697d47d0
overH <2262135195@qq.com>
2024-04-22 17:50:55 +0800

    log4j2日志配置和mybatis-plus控制台输出日志完成

--- cloud-provider-payment8001/src/main/java/com/overthinker/cloud/Main8001.java
+++ cloud-provider-payment8001/src/main/java/com/overthinker/cloud/Main8001.java
@@ -1,13 +1,19 @@
 package com.overthinker.cloud;
 
 
+import lombok.extern.log4j.Log4j2;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.overthinker.cloud.mapper")
+@Log4j2
 public class Main8001 {
+//    当项目启动时控制台输出项目启动成功的日志
+    static {
+        log.info("项目启动成功");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main8001.class, args);
