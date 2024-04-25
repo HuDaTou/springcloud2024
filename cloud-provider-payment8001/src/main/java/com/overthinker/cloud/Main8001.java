0a08b80f55d1e58814d1404c16a43bdcabc78b4d
overH <2262135195@qq.com>
2024-04-25 16:29:35 +0800

    consul分布式集中配置管理

--- cloud-provider-payment8001/src/main/java/com/overthinker/cloud/Main8001.java
+++ cloud-provider-payment8001/src/main/java/com/overthinker/cloud/Main8001.java
@@ -6,11 +6,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @MapperScan("com.overthinker.cloud.mapper")
 @Log4j2
 @EnableDiscoveryClient
+@RefreshScope
 public class Main8001 {
 //    当项目启动时控制台输出项目启动成功的日志
     static {
