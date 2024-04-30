b96833f1c0d292ad5c000ed19fc5c06d4dad48c6
overH <2262135195@qq.com>
2024-04-30 17:55:42 +0800

    CircuitBreaker #信号量舱壁 实现SemaphoreBulkhead

--- cloud-consumer-order80/src/main/java/com/overthinker/cloud/controller/OrderCircuitController.java
+++ cloud-consumer-order80/src/main/java/com/overthinker/cloud/controller/OrderCircuitController.java
@@ -1,6 +1,7 @@
 package com.overthinker.cloud.controller;
 
 import com.overthinker.cloud.apis.PayFeignApi;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
@@ -22,5 +23,22 @@ public class OrderCircuitController {
     public String myCircuitFallback(Integer id,Throwable t) {
         // 这里是容错处理逻辑，返回备用结果
         return "myCircuitFallback，系统繁忙，请稍后再试-----/(ㄒoㄒ)/~~";
+    }
+
+    /**
+     *(船的)舱壁,隔离
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/feign/pay/bulkhead/{id}")
+//    TODO:这个注解什么用
+    @Bulkhead(name = "cloud-payment-service",fallbackMethod = "myBulkheadFallback",type = Bulkhead.Type.SEMAPHORE)
+    public String myBulkhead(@PathVariable("id") Integer id)
+    {
+        return payFeignApi.myBulkhead(id);
+    }
+    public String myBulkheadFallback(Throwable t)
+    {
+        return "myBulkheadFallback，隔板超出最大数量限制，系统繁忙，请稍后再试-----/(ㄒoㄒ)/~~";
     }
 }
