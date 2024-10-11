6caa22c6c3dc71db85b3d93485479e900b8784e4
overH <2262135195@qq.com>
2024-10-11 18:08:21 +0800

    提交到test3

---
+++ cloud-media/src/main/java/com/overthinker/cloud/cloudmedia/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.overthinker.cloud.cloudmedia.controller;
+
+public class UserController {
+//    测试打印一个hello word
+    public void hello() {
+        System.out.println("hello word");
+    }
+}
