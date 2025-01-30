ada9070a2dff5b0e360c04040202ad3a37ceeffb
HuDaTou <overhyyds@gmail.com>
2025-01-30 11:24:50 +0800

    项目分模块了

--- backend/cloud-media/media-api/src/main/java/com/overthinker/cloud/media_api/controller/BigFilesController.java
+++
@@ -1,9 +0,0 @@
-package com.overthinker.cloud.media_api.controller;
-
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@Tag(name = "媒资文件管理接口")
-public class BigFilesController {
-}
