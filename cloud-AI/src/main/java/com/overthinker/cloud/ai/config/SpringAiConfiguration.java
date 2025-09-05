26edd5123c2e0780ad2c0de7a5e69250e2b4b331
HuDaTou <overhyyds@gmail.com>
2025-09-05 14:45:56 +0800

    修改 devcontainer 的容器打包内容

--- cloud-AI/src/main/java/com/overthinker/cloud/ai/config/SpringAiConfiguration.java
+++ cloud-AI/src/main/java/com/overthinker/cloud/ai/config/SpringAiConfiguration.java
@@ -2,7 +2,7 @@ package com.overthinker.cloud.ai.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.ollama.OllamaChatModel;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,18 +17,18 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class SpringAiConfiguration {
 
-    /**
-     * 创建并配置一个 {@link ChatClient} Bean。
-     * <p>
-     * {@link ChatClient} 是与大语言模型进行交互的推荐方式，它提供了一个流畅的、
-     * 方便的 API 来构建和发送请求。
-     * </p>
-     *
-     * @param ollamaChatModel Spring Boot 自动配置的 Ollama 聊天模型实例。
-     * @return 一个配置好的 {@link ChatClient} 实例，可用于应用程序的其他部分。
-     */
-    @Bean
-    public ChatClient anthropicChatClient(OllamaChatModel ollamaChatModel) {
-        return ChatClient.create(ollamaChatModel);
-    }
+//    /**
+//     * 创建并配置一个 {@link ChatClient} Bean。
+//     * <p>
+//     * {@link ChatClient} 是与大语言模型进行交互的推荐方式，它提供了一个流畅的、
+//     * 方便的 API 来构建和发送请求。
+//     * </p>
+//     *
+//     * @param ollamaChatModel Spring Boot 自动配置的 Ollama 聊天模型实例。
+//     * @return 一个配置好的 {@link ChatClient} 实例，可用于应用程序的其他部分。
+//     */
+//    @Bean
+//    public ChatClient anthropicChatClient(OllamaChatModel ollamaChatModel) {
+//        return ChatClient.create(ollamaChatModel);
+//    }
 }
